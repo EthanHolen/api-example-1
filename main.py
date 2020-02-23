40e26ddb9043f71ef11378547cc0e2b5879afaf5
Ethan Holen <ethanholen@gmail.com>
2020-02-23 10:24:02 -0700

    fixed int in fibo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 def fibo(n):
+    n = int(n)
     if n == 0: return 0
     elif n == 1: return 1
     else: return fibo(n-1)+ fibo(n-2)
@@ -17,4 +18,3 @@ def prequel():
         "I have the POWER! UNLIMITED… POWER!",
         "I’ll try spinning. That’s a good trick. Whoa-ah!"]
     return random.choice(quotes)
-
