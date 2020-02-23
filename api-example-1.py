da7c5b1614dcdc4036630db79dc8a61f6b1f21f0
Ethan Holen <ethanholen@gmail.com>
2020-02-23 09:28:25 -0700

    prequel function

---
+++ api-example-1.py
@@ -0,0 +1,20 @@
+import random
+
+def fibo(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fibo(n-1)+ fibo(n-2)
+
+def sayHello(name):
+    return "Hello " + name + "!"
+
+def prequel():
+    quotes = [
+        "Don’t try it, Anakin. I have the high ground!", 
+        "There’s always a bigger fish.", 
+        "I don’t like sand. It’s coarse and rough and irritating and it gets everywhere.", 
+        "I’ve been wondering… what are midi-chlorians?", 
+        "I have the POWER! UNLIMITED… POWER!",
+        "I’ll try spinning. That’s a good trick. Whoa-ah!"]
+    return random.choice(quotes)
+
